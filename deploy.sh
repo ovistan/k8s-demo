b328929fa5243b52f9aaf56c862cc7164a096bd5
ovidiu.stan <stanovidiuioan@gmail.com>
2020-05-06 01:45:22 +0300

    fix deployment

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ docker push ovist/section14-client:$SHA
 docker push ovist/section14-server:$SHA
 docker push ovist/section14-worker:$SHA
 
-kubectl -f apply ./k8s
-kubectl set image deployments/client-deployment client=ovist/section14-client:$SHA
-kubectl set image deployments/server-deployment server=ovist/section14-server:$SHA
-kubectl set image deployments/worker-deployment worker=ovist/section14-worker:$SHA
+kubectl apply -f ./k8s
+kubectl set image deployment/client-deployment client=ovist/section14-client:$SHA
+kubectl set image deployment/server-deployment server=ovist/section14-server:$SHA
+kubectl set image deployment/worker-deployment worker=ovist/section14-worker:$SHA
